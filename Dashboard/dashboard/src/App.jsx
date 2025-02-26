dcf8721a6299ac9e1ff00a3ede5559bc78ac1008
Thorfast191 <ti2431267@gmail.com>
2025-02-26 21:29:04 +0600

    Dashboard Setup

---
+++ Dashboard/dashboard/src/App.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import HomePage from "./Pages/HomePage.jsx";
+import Login from "./Pages/Login.jsx";
+import ForgotPassword from "./Pages/ForgotPassword.jsx";
+import ResetPassword from "./Pages/ResetPassword.jsx";
+import ManageSkills from "./Pages/ManageSkills.jsx";
+import ManageTimeline from "./Pages/ManageTimeline.jsx";
+import ManageProjects from "./Pages/ManageProjects.jsx";
+import ViewProject from "./Pages/ViewProject.jsx";
+import UpdateProject from "./Pages/UpdateProject.jsx";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/password/forgot" element={<ForgotPassword />} />
+        <Route path="/password/reset/:token" element={<ResetPassword />} />
+        <Route path="/manage/skills" element={<ManageSkills />} />
+        <Route path="/manage/timeline" element={<ManageTimeline />} />
+        <Route path="/manage/projects" element={<ManageProjects />} />
+        <Route path="/view/project/:id" element={<ViewProject />} />
+        <Route path="/update/project/:id" element={<UpdateProject />} />
+      </Routes>
+      <ToastContainer position="top-right" theme="dark" />
+    </Router>
+  );
+};
+
+export default App;
